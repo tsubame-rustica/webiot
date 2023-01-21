07bc2b9f3d46391db934e02b9ab9de0f63260351
tsubame-rustica <120567038+tsubame-rustica@users.noreply.github.com>
2023-01-21 17:37:57 +0900

    Add files via upload

---
+++ distance/pc.js
@@ -0,0 +1,23 @@
+// Remote Example1 - controller
+
+var channel;
+onload = async function () {
+  // webSocketリレーの初期化
+  var relay = RelayServer("chirimentest", "chirimenSocket");
+  channel = await relay.subscribe("chirimenSHT");
+  messageDiv.innerText = " web socketリレーサービスに接続しました";
+  channel.onmessage = getMessage;
+};
+
+function getMessage(msg) {
+  // メッセージを受信したときに起動する関数
+  var mdata = msg.data;
+  messageDiv.innerText = JSON.stringify(mdata);
+  console.log("mdata:", mdata);
+  temTd.innerText = mdata;
+}
+
+function getData() {
+  // get microbit's internal sensor data
+  channel.send("GET SENSOR DATA");
+}
